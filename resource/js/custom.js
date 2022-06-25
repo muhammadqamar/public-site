d55bce75374578a4994b3b746f08217388b3ee02
muhammadqamar <muhammadqamar111@gmail.com>
2022-06-25 15:07:00 +0500

    update faq

--- resource/js/custom.js
+++ resource/js/custom.js
@@ -9,3 +9,16 @@ $(".banner_slider2").slick({
   centerMode: true,
   centerPadding: "60px",
 });
+
+$(".logo_slider3").slick({
+  dots: false,
+  infinite: true,
+  autoplay: true,
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  arrows: false,
+  variableWidth: true,
+  speed: 2000,
+  autoplaySpeed: 2000,
+  cssEase: "linear",
+});
